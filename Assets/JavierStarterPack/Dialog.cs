15267d663d51b683f39e667335f62b1bc5d80e89
frezz23 <racketpacket@gmail.com>
2016-05-21 12:00:32 -0400

    Final Project

---
+++ Assets/JavierStarterPack/Dialog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+	public GUIStyle style = new GUIStyle(); //I made the GUI Style public so you can add customizations to your Text Box
+	public bool dialog;
+	public string words;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") { // If I enter the dialog cube
+			dialog = true; //Set dialog to true so the GUI box can work
+		}
+	}
+	void OnTriggerExit(Collider other){ //If I leave dialog cube
+		dialog = false; //Set dialog to false so the GUI box can disappear
+	}
+
+	void OnGUI(){
+		if (dialog == true) {
+			GUI.Box (new Rect (Screen.width / 1000, Screen.height / 2, 1000, 20), words, style); //You can mess around with this if you like.
+			//Rect API is new Rect(position x, position y, the width of the rect and the height of the rect) Very simple
+		}
+	}
+}
---
+++ Assets/JavierStarterPack/README.txt
@@ -0,0 +1,12 @@
+Welcome to your starter pack. Since your game is revolved around Dialogue, it will be fairly easy to make. If there is anything else you want to your game, go ahead I won't stop you. Just make sure it is doable before Presentations.
+
+Now in your starter pack, I have included:
+* A land prefab
+* Character walking script
+* A character
+* The Dialog script and a prefab
+* An example scene on how to use the scripts together
+
+The land is easy to make with Unity. You can go into the asset store and pull in trees and for the ground it self, you can just google "Seamless mountain ground" and download the PNG you want.
+
+Good luck making you game. If anything, contact me or Jeands. Have fun!
